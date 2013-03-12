770c5ac25542b870c01b83414d7bcf2470f63940
diegomart <judimay_18@hotmail.com>
2013-03-12 09:13:24 -0600

    CAMBIOS CLASE PERSONA

--- practica 3/practica3/Persona.cs
+++ practica 3/practica3/Persona.cs
@@ -7,5 +7,19 @@ namespace practica3
 {
     class Persona
     {
+
+        public string codigo;
+		public string nombre;
+		public string direccion;
+		public string facebook;
+		public string telefono;
+
+		public Persona(){
+			this.codigo = "";
+			this.nombre = "";
+			this.direccion = "";
+			this.facebook = "";
+			this.telefono = "";
+
     }
 }
--- practica 3/practica3/Principal.cs
+++ practica 3/practica3/Principal.cs
@@ -7,5 +7,8 @@ namespace practica3
 {
     class Principal
     {
+
+
+
     }
 }
